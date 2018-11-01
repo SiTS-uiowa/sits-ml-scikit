9aa0db5dfc08f5ad4242522f7e3aadc8288f8b32
Alic Szecsei <aszecsei@gmail.com>
2018-11-01 18:09:46 -0500

    Add ML step and cross-validation

--- app.py
+++ app.py
@@ -36,7 +36,22 @@ steps = [year_si_step, kbd_step]
 year_pipe = Pipeline(steps)
 year_cols = ['Year']
 year_train = data_file[year_cols]
-print(year_train.head(3))
+# print(year_train.head(3))
 
 year_train_transformed = year_pipe.fit_transform(year_train)
-print("Num per bin:", year_train_transformed.sum(axis=0))
+# print("Num per bin:", year_train_transformed.sum(axis=0))
+
+transformers = [('string', pipe, string_cols),
+                ('year', year_pipe, year_cols)]
+ct = ColumnTransformer(transformers=transformers)
+X = ct.fit_transform(data_file)
+# print("Shape:", X.shape)
+
+ml_pipe = Pipeline([('transform', ct), ('ridge', BayesianRidge())])
+ml_pipe.fit(data_file, y)
+
+# print("Score:", ml_pipe.score(data_file, y))
+
+from sklearn.model_selection import KFold, cross_val_score
+kf = KFold(n_splits=5, shuffle=True, random_state=1337)
+print("Score:", cross_val_score(ml_pipe, data_file, y, cv=kf).mean())
